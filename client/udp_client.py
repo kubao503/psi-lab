347c1aea8238f32e618ae6cbcf16cf28f6f77f9a
Jakub Stacherski <o503@interia.eu>
2023-11-24 15:50:15 +0100

    Fixed formatting

--- client/udp_client.py
+++ client/udp_client.py
@@ -5,31 +5,31 @@ import socket
 import sys
 import io
 
-HOST = 'server' #'127.0.0.1'  # The server's hostname or IP address
+HOST = "server"
 size = 1
 binary_stream = io.BytesIO()
 
-if  len(sys.argv) < 2:
-  print("no port, using 8000")
-  port=8000
+if len(sys.argv) < 2:
+    print("no port, using 8000")
+    port = 8000
 else:
-  port = int( sys.argv[1] )
+    port = int(sys.argv[1])
 
 print("Will send to ", HOST, ":", port)
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-  while True:
-#    buffer = str( size )
-    binary_stream.write("Hello, world!\n".encode('ascii'))
-    binary_stream.seek(0)
-    stream_data = binary_stream.read()
-    # print( "Sending buffer size= ", size, "data= ", stream_data  )
-    print( "Sending buffer size= ", size  )
+    while True:
+        #    buffer = str( size )
+        binary_stream.write("Hello, world!\n".encode("ascii"))
+        binary_stream.seek(0)
+        stream_data = binary_stream.read()
+        # print( "Sending buffer size= ", size, "data= ", stream_data  )
+        print("Sending buffer size= ", size)
 
-    s.sendto( stream_data, (HOST, port)    )
-    data = s.recv( size )
-    # print('Received', repr(data))
-    size = size * 2
+        s.sendto(stream_data, (HOST, port))
+        data = s.recv(size)
+        # print('Received', repr(data))
+        size = size * 2
 
 
-print('Client finished.')
+print("Client finished.")
