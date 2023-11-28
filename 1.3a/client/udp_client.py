5707fe46ffa42e1672296cdcbe4471af54733376
Kozprog <maciejmaciejkoz@gmail.com>
2023-11-28 22:30:57 +0100

    Was no good, is good now!

---
+++ 1.3a/client/udp_client.py
@@ -0,0 +1,32 @@
+# simple Python socket client
+# (c) G.Blinowski for PSI 2021
+
+import socket
+import math
+import sys
+from net_dict_sender import NetDictSender
+
+
+HOST = "server"
+
+if len(sys.argv) < 2:
+    print("no port, using 8000")
+    port = 8000
+else:
+    port = int(sys.argv[1])
+
+print("Will send to ", HOST, ":", port)
+
+data = {}
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    datagram = NetDictSender(data)
+    for i in range(100):
+        num_elements = math.ceil(9570 * 0.5**(i/2))
+        last_num = len(data)
+        print(num_elements)
+        data.update({f"{last_num+j:03d}": f"{last_num+j:03d}" for j in range(num_elements)})
+        datagram.dictobj = data
+        datagram.sendto(sock, (HOST, port))
+
+print("Client finished.")
