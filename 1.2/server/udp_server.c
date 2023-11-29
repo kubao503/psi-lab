f6f0847912e31834eb496198ff4edfcc27bb034e
Maciej Kozłowski <67715928+kozlowskimaciej@users.noreply.github.com>
2023-11-29 21:33:07 +0100

    Update udp_server.c

--- 1.2/server/udp_server.c
+++ 1.2/server/udp_server.c
@@ -4,7 +4,6 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-
 #define BUFSIZE 1024
 
 int main(int argc, char *argv[])
@@ -21,35 +20,23 @@ int main(int argc, char *argv[])
         port = atoi(argv[1]);
     }
 
-
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) 
-    {
-        perror("socket call error");
-    }
+
+    printf("Will listen on %s : %d\n", inet_ntoa(server_addr.sin_addr), port);
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port);
 
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
-    {
-        perror("bind");
-        printf("Error binding socket with address!\n");
-    }
-
-    printf("Will listen on %s:%d\n", inet_ntoa(server_addr.sin_addr), port);
+    bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
     char buffer[BUFSIZE];
     while (1)
     {
         int bytes_received = recvfrom(sockfd, buffer, BUFSIZE, 0, (struct sockaddr *)&client_addr, &addr_len);
-
-        printf("%d\n", bytes_received);
-        int bytes_received_network = htonl(bytes_received);
 
         if (bytes_received <= 0)
         {
@@ -96,7 +83,7 @@ int main(int argc, char *argv[])
         }
         printf("}\n");
 
-        int send_res = sendto(sockfd, &bytes_received_network, sizeof(bytes_received_network), 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
+        int send_res = sendto(sockfd, &bytes_received, sizeof(bytes_received), 0, (struct sockaddr *)&client_addr, sizeof(client_addr));
         if (send_res == -1)
         {
             perror("sendto");
@@ -107,6 +94,8 @@ int main(int argc, char *argv[])
         free(values);
     }
 
-    close(sockfd);
+    if (close(sockfd) == -1) {
+        printf("Error closing socket!\n");
+    }
     return 0;
 }
