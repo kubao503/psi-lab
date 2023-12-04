8f1f3acc09ba8270abc23ca640e1b26bd9442c2f
Jakub Stacherski <o503@interia.eu>
2023-12-04 23:10:53 +0100

    Printing paths from root to all leaves

--- lab2/2.1/client/tcp_client.c
+++ lab2/2.1/client/tcp_client.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_DEPTH 20
+
 struct Node {
     struct Node *child_left;
     struct Node *child_right;
@@ -15,14 +17,39 @@ struct Node *allocate_node(char *text) {
 }
 
 struct Node *get_binary_tree() {
-    struct Node *root = allocate_node(".");
-    root->child_left = allocate_node("com");
+    struct Node *root = allocate_node("");
+    root->child_left = allocate_node(".com");
+    root->child_right = allocate_node(".pl");
+    root->child_right->child_left = allocate_node(".edu");
+    root->child_right->child_left->child_left = allocate_node(".pw");
 
     return root;
+}
+
+void print_tree_paths_rec(struct Node *node, char *path_texts[], int path_texts_len) {
+    if (!node)
+        return;
+
+    path_texts[path_texts_len] = node->text;
+
+    if (!node->child_left && !node->child_right) {
+        for (int i = path_texts_len; i >= 0; --i)
+            printf("%s", path_texts[i]);
+        printf("\n");
+        return;
+    }
+
+    print_tree_paths_rec(node->child_left, path_texts, path_texts_len + 1);
+    print_tree_paths_rec(node->child_right, path_texts, path_texts_len + 1);
+}
+
+void print_tree_paths(struct Node *root) {
+    char *path_texts[MAX_DEPTH];
+    print_tree_paths_rec(root, path_texts, 0);
 }
 
 int main(int argc, char *argv[]) {
     struct Node *root = get_binary_tree();
 
-    printf("%s %s\n", root->text, root->child_left->text);
+    print_tree_paths(root);
 }
