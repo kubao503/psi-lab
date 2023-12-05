7abafc9c6ea0f035b78783047c332b7b9c8ca5a6
Jakub Stacherski <o503@interia.eu>
2023-12-05 12:34:40 +0100

    Writing tree to a buffer

--- lab2/2.1/client/tcp_client.c
+++ lab2/2.1/client/tcp_client.c
@@ -17,7 +17,7 @@ struct Node *allocate_node(char *text) {
     node->text = text;
 }
 
-struct Node *get_binary_tree() {
+struct Node *get_tree_example() {
     struct Node *root = allocate_node("");
     root->child_left = allocate_node(".com");
     root->child_right = allocate_node(".pl");
@@ -49,18 +49,58 @@ void print_tree_paths(struct Node *root) {
     print_tree_paths_rec(root, path_texts, 0);
 }
 
-size_t get_tree_size(struct Node *node, size_t total_size) {
+size_t get_tree_size_rec(struct Node *node, size_t total_size) {
     if (!node)
         return total_size;
 
-    total_size = get_tree_size(node->child_left, total_size);
-    total_size = get_tree_size(node->child_right, total_size);
+    total_size = get_tree_size_rec(node->child_left, total_size);
+    total_size = get_tree_size_rec(node->child_right, total_size);
     return total_size + 3 * sizeof(unsigned) + strlen(node->text);
 }
 
+size_t get_tree_size(struct Node *root) {
+    return get_tree_size_rec(root, 0);
+}
+
+int write_tree_to_buf_rec(struct Node *node, void **buf, int *index) {
+    if (!node)
+        return -1;
+
+    int index_left = write_tree_to_buf_rec(node->child_left, buf, index);
+    int index_right = write_tree_to_buf_rec(node->child_right, buf, index);
+
+    size_t str_len = strlen(node->text);
+    memcpy(*buf, &str_len, 4);
+    memcpy(*buf + 4, node->text, str_len);
+
+    memcpy(*buf + 4 + str_len, &index_left, 4);
+    memcpy(*buf + 8 + str_len, &index_right, 4);
+
+    for (int i = 0; i < 12 + str_len; ++i)
+        printf("%d ", ((char *)*buf)[i]);
+    printf("\n");
+
+    *buf += 12 + str_len;
+    *index += 1;
+    return *index;
+}
+
+void write_tree_to_buf(struct Node *root, void *buf) {
+    int index = -1;
+    write_tree_to_buf_rec(root, &buf, &index);
+}
+
 int main(int argc, char *argv[]) {
-    struct Node *root = get_binary_tree();
+    struct Node *root = get_tree_example();
 
     print_tree_paths(root);
-    printf("Tree size: %lu\n", get_tree_size(root, 0));
+    size_t tree_size = get_tree_size(root);
+    void *buf = malloc(tree_size);
+    write_tree_to_buf(root, buf);
+
+    for (int i = 0; i < tree_size; ++i)
+        printf("%d ", ((char *)buf)[i]);
+    printf("\n");
+
+    free(buf);
 }
